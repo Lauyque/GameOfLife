64603b98af7e3784f985c17c3ff0f55e2a5b8075
Drekstord <matteo2010@outlook.fr>
2025-01-17 13:14:01 +0100

    Retrait des fonctions de sauvegarde etc, pour recommencer au propre

--- sauvegarde.h
+++
@@ -1,7 +0,0 @@
-#ifndef SAUVEGARDE_H
-#define SAUVEGARDE_H
-
-void sauvegarder_grille(int** grille, int largeur, int hauteur, const char* nom_fichier);
-void charger_grille(int** grille, int largeur, int hauteur, const char* nom_fichier);
-
-#endif // SAUVEGARDE_H
